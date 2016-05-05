3d95a60a2fd7ae4b034ef5cd3b8f7f2eacf522ec
chengbin <chengbinz@jumei.com>
2016-05-05 20:26:18 +0800

    增加例子

--- CBRedDot/ViewController.m
+++ CBRedDot/ViewController.m
@@ -18,16 +18,64 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
-    UIView *view = [[UIView alloc]init];
     
-    view.frame = CGRectMake(100, 200, 100, 100);
-    view.backgroundColor = [UIColor orangeColor];
+    UIView *instanceView = [[UIView alloc]init];
+    instanceView.frame = CGRectMake(100, 100, 100, 100);
+    instanceView.backgroundColor = [UIColor orangeColor];
     
-    [self.view addSubview:view];
+    [self.view addSubview:instanceView];
     
-    CBRedDot *redDot  = [[CBRedDot alloc]init];
+    UIView *instanceView2 = [[UIView alloc]init];
+    instanceView2.frame = CGRectMake(250, 100, 100, 100);
+    instanceView2.backgroundColor = [UIColor greenColor];
     
-    [view addSubview:redDot];
+    [self.view addSubview:instanceView2];
+    
+    UIView *instanceView3 = [[UIView alloc]init];
+    instanceView3.frame = CGRectMake(100, 300, 100, 100);
+    instanceView3.backgroundColor = [UIColor brownColor];
+    
+    [self.view addSubview:instanceView3];
+    
+    UIView *instanceView4 = [[UIView alloc]init];
+    instanceView4.frame = CGRectMake(250, 300, 100, 100);
+    instanceView4.backgroundColor = [UIColor blueColor];
+    
+    [self.view addSubview:instanceView4];
+    
+    UIView *instanceView5 = [[UIView alloc]init];
+    instanceView5.frame = CGRectMake(250, 500, 100, 100);
+    instanceView5.backgroundColor = [UIColor blueColor];
+    
+    [self.view addSubview:instanceView5];
+    
+    //创建小红点 并添加到 视图上
+    [instanceView addSubview:[[CBRedDot alloc]init]];
+    
+    CBRedDot *redDot2 = [[CBRedDot alloc]init];
+    redDot2.verticalAlignment = CBRedDotViewVerticalAlignmentMiddle;
+    redDot2.horizontalAlignment = CBRedDotViewHorizontalAlignmentRight;
+    [instanceView2 addSubview:redDot2];
+    
+    
+    CBRedDot *redDot3 = [[CBRedDot alloc]init];
+    redDot3.verticalAlignment = CBRedDotViewVerticalAlignmentBottom;
+    redDot3.horizontalAlignment = CBRedDotViewHorizontalAlignmentRight;
+    [instanceView3 addSubview:redDot3];
+    
+    
+    CBRedDot *redDot4 = [[CBRedDot alloc]init];
+    redDot4.verticalAlignment = CBRedDotViewVerticalAlignmentTop;
+    redDot4.horizontalAlignment = CBRedDotViewHorizontalAlignmentLeft;
+    [instanceView4 addSubview:redDot4];
+    
+    
+    CBRedDot *redDot5 = [[CBRedDot alloc]init];
+    redDot5.verticalAlignment = CBRedDotViewVerticalAlignmentMiddle;
+    redDot5.horizontalAlignment = CBRedDotViewHorizontalAlignmentLeft;
+    [instanceView5 addSubview:redDot5];
+    
+    
     
 }
 
